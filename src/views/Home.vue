404af32e0f6c9e45db1a837267e7adeec00f1821
palmdaldaniel <daniel.palmdal@gmail.com>
2021-10-10 20:33:59 +0200

    set a global interval variable to break the scanningprogress function when it reaches 100

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,11 +10,9 @@
       <button v-on:click="getScanningProgres">Get Progress</button>
       <button v-on:click="whoAmI">Who am i ?</button>
 
-  <div class="progress">
-    <div :style="{ width: value + '%' }" class="progressBar">
-    </div>
-  </div>
-
+      <div class="progress">
+        <div :style="{ width: value + '%' }" class="progressBar"></div>
+      </div>
     </div>
   </div>
 </template>
@@ -22,7 +20,7 @@
 import { store } from "../store/store.js";
 
 const token =
-  "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzUxMiJ9.eyJpYXQiOjE2MzM4NzI0MTksImV4cCI6MTYzMzg3NjAxOSwicm9sZXMiOlsiUk9MRV9PUEVOX0FQSSIsIlJPTEVfVVNFUiIsIlJPTEVfQ09NUEFOWV9BRE1JTiJdLCJ1c2VybmFtZSI6IjdmYTZkODBmZjVkMmYyM2EzZjY0Mjk5YTU2N2EzOWUwYTRmZDE2ZjYifQ.BftQuYhy9s1fzHxLP0qQrrznbvPPSrfF-Ej7xiAqxn9CWLXI1a-pXV7YIgMdGTpPhU0RJcGwUcbyC1IW--PTSc9E8A20Fw6bRrPWFLGu1UaAqwBG5rpk_JXWL27nZ9rwx9lJ28FhrBTHFNbrV00EyrtuETLdU_KmahxW-QzVXBdkfnwfV0CsRon8Exf8o9X9OyjODdoroD0rVu-drC6sF_bnQ4qx8onOJS__yGjm8kHV-Z7eY-9-wm_IOxIaIVyzU6TFVAP8L3EJ1B8qgxt-N2T8C6OcsE4FKLkDqHxlmytZVnMcwSfOHUQglkB4BX4uDEjYkgpGAYGZIRpqtPWJ8EDlz2z51tPTZZ_S4qnpy0LNGNRv3HVFnHAGnKykEiH7G1URgBhn14zg6HXeQkvNwfCzYeIkRgxfh26RHrJyGasQWJw_Y-6R3nIDGNSgQ5p9Wp9THSAGQBfZrllVvWrUoGs5LF7x9lhcslEJbWxVMfBDNtqzi6SX01hWJouZO0inVDIo-gvIc_lkgsWwfigcAkIOIYCk9TnXs2Jxe58NziioQU7X6hy2CKQJl4YWuIvl40DaUEupyw3NC5BK_B27mkr5_AVs1t_eVzfZe0q0EO-wr2stfbxr1qq_hblIPI1tsaw-IhYuhoWT3utzGfdxngeIBRGXtW4X4AbK1Vo6Juo";
+  "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzUxMiJ9.eyJpYXQiOjE2MzM4OTA2NDksImV4cCI6MTYzMzg5NDI0OSwicm9sZXMiOlsiUk9MRV9PUEVOX0FQSSIsIlJPTEVfVVNFUiIsIlJPTEVfQ09NUEFOWV9BRE1JTiJdLCJ1c2VybmFtZSI6IjdmYTZkODBmZjVkMmYyM2EzZjY0Mjk5YTU2N2EzOWUwYTRmZDE2ZjYifQ.Y5JD5a-98H-7Pvbzi_Npviym7ApJC1pKG_EGmdgMuLjyv-JZsCa-pBTqGtu5v68TXd7H_GIctObcMBRhtLoEyR2STilDoRqgVg6ALGc6B8FW3sArEyWPHCy3yQzYDwOfvdETOF1JkegRF8qSWGP-7WIa2CWfiIfHq5xIVn16CgA4n2iYqr1-0ID03OExFiX71qe_GdZO4HOMDfjfVBan--YqAh4b5mH-luzAMl19eBoT2RHxAK3l2G6CDaRMv4wE6yB0-hTKlASqnifvwd4d4Ko9SxRJ1W-HNlTV1gpKEqUuQS0inHO7tCo6hStlS5pYpvXA_om0to9amVlL2aOAyXzNeGtmxwU_WqvX3MGZe7KG-VsOWYUWke4e8qc5jpxMccjhT4RfeA2oM6PtWgC97fXfZvTZkePESk82B0X7HPTetRvhX8dO9mKFqIg9Xqj7hTviqiKPeBZXXC3y1HowouVvM4ihMUcd8JXXZgjx3lM1v5xyUOLvVsp3ygXMHPSuePXMQlMYRXl3f96Ys3p10rthey8By6NyaKcHXewVAmJJC7Pw4DFChIn3JiNhCNxY8DUfWsewUCskYfCUD0gAkiqq-vmQy29KPZ5SmMTzwelAir4gZtwl6tupWZhAPx6GEnRbZrQipIT_sDE104mB7_y1SiOSP02imsQVDb_iOpo";
 
 // submit dependency file // post request
 const urlUpload =
@@ -36,15 +34,17 @@ const urlStartScanning =
 const urlTrackProgress =
   "http://localhost:8081/api/1.0/open/ci/upload/status?ciUploadId=2203480";
 
-const urlWhoAmI = "http://localhost:8081/api/1.0/open/zapier/user"
+const urlWhoAmI = "http://localhost:8081/api/1.0/open/zapier/user";
+
+let interval = 0;
 
 export default {
   data() {
     return {
-      id: 2186456,
+      id: null,
       file: null,
       useTheme: store.state.useTheme,
-      value: 0
+      value: 0,
     };
   },
   methods: {
@@ -68,49 +68,45 @@ export default {
           /*   onUploadProgress: (progressEvent) => {}, */
         });
         console.log("data", res.data);
+
+        this.id = res.data.ciUploadId;
       } catch (error) {
         console.log("error", error);
       }
     },
-    async startScanning() { 
-      console.log('start scanning');
-        const formData = new FormData();
-        formData.append('ciUploadId', "2203480")
+    async startScanning() {
+      console.log("start scanning");
+      const formData = new FormData();
+      formData.append("ciUploadId", this.id);
 
       try {
-        const res = await axios.post(urlStartScanning, formData,
-         {
+        const res = await axios.post(urlStartScanning, formData, {
           headers: {
             Authorization: "Bearer " + token,
           },
         });
-        console.log("data", res);
+        // if post req returns a 204 fire an interval to get updates about scanning progress every 1.5 sec.
+        interval = setInterval(this.getScanningProgres(), 1500);
       } catch (error) {
         console.log("error", error);
       }
     },
-    interval() {
-      let inter = setInterval(this.getScanningProgres, 3000);
-    },
-    async getScanningProgres() {
 
-      console.log('get scan progress');
+    async getScanningProgres() {
+      console.log("get scan progress");
       try {
         const res = await axios.get(urlTrackProgress, {
           headers: {
             Authorization: "Bearer " + token,
           },
         });
-        //console.log('id', res.data.ciUploadId);
-       
-        console.log("progress:>", res);
+        this.value = res.data.progress;
       } catch (error) {
         console.log("error", error);
       }
     },
     async whoAmI() {
-
-         try {
+      try {
         const res = await axios.get(urlWhoAmI, {
           headers: {
             Authorization: "Bearer " + token,
@@ -121,13 +117,23 @@ export default {
       } catch (error) {
         console.log("error", error);
       }
-
-
-    }
+    },
+  },
+  watch: {
+    // when id is set start the scan progress!
+    id: function (val) {
+      if (val === this.id) {
+        this.startScanning();
+      }
+    },
+    value: function (val) {
+      console.log(val);
+      clearInterval(interval)
+    },
   },
 };
 </script>
-<style scoped > 
+<style scoped>
 .wrapper {
   height: 100vh;
 }
